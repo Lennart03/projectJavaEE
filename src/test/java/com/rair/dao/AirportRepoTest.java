6db209e1cdfd3f2866092167b244273132d281ec
Niels Stassijns <niels.stassijns@realdolmen.com>
2016-11-07 10:53:21 +0100

    tests voor airport en partnerbeheer

--- src/test/java/com/rair/dao/AirportRepoTest.java
+++ src/test/java/com/rair/dao/AirportRepoTest.java
@@ -2,6 +2,8 @@ package com.rair.dao;
 
 import java.util.List;
 
+import javax.validation.ConstraintViolationException;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -29,15 +31,53 @@ public class AirportRepoTest extends JpaPersistenceTest{
 		assertNotNull(airport);		
 	}
 	
+	@Test(expected=ConstraintViolationException.class)
+	public void airportCanBePersistedWithoutName() throws Exception{
+		Airport airport = new Airport();
+		airport.setAirportCode("test");
+		airport.setCountry("test");
+		airport.setRegion(Region.AFRICA);
+		airport = airportRepository.save(airport);
+		assertNotNull(airport);		
+	}
+	
+	@Test(expected=ConstraintViolationException.class)
+	public void airportCanBePersistedWithoutCountry() throws Exception{
+		Airport airport = new Airport();
+		airport.setAirportCode("test");
+		airport.setName("test");
+		airport.setRegion(Region.AFRICA);
+		airport = airportRepository.save(airport);
+		assertNotNull(airport);		
+	}
+	
+	@Test(expected=ConstraintViolationException.class)
+	public void airportCanBePersistedWithoutCode() throws Exception{
+		Airport airport = new Airport();
+		airport.setName("test");
+		airport.setCountry("test");
+		airport.setRegion(Region.AFRICA);
+		airport = airportRepository.save(airport);
+		assertNotNull(airport);		
+	}
+	
 	@Test
 	public void airportCanBeRemoved() throws Exception{
 		Airport airport = new Airport("testNaam", "testLand", Region.EUROPE, "testCode");
-		Airport airport2 = new Airport("testNaam", "testLand", Region.EUROPE, "testCode");
+		Airport airport2 = new Airport("testNaam2", "testLand", Region.EUROPE, "testCode");
 		airportRepository.save(airport);
 		airportRepository.save(airport2);
-		airportRepository.remove(2);
+		airportRepository.remove(airport2.getId());
 		List<Airport> airportList = airportRepository.findAll();
-		assertEquals(1, airportList.size());		
+		assertEquals(7, airportList.size());		
+	}
+	
+	@Test
+	public void existingAirportCanBeRemoved() throws Exception{
+		Airport airport = airportRepository.findById(1L);
+		airportRepository.remove(airport.getId());
+		List<Airport> airportList = airportRepository.findAll();
+		assertEquals(5, airportList.size());		
 	}
 	
 	@Test
--- src/test/java/com/rair/dao/PartnerRepoTest.java
+++ src/test/java/com/rair/dao/PartnerRepoTest.java
@@ -1,6 +1,10 @@
 package com.rair.dao;
 
 
+import java.util.List;
+
+import javax.validation.ConstraintViolationException;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -26,14 +30,80 @@ public class PartnerRepoTest extends JpaPersistenceTest{
 		assertNotNull(partner);		
 	}
 	
-//	@Test
-//	public void airportCanBeRemoved() throws Exception{
-//		//do something		
-//	}
-//	
-//	@Test
-//	public void airportCanBeUpdated() throws Exception{
-//		//do something
-//	}
+	@Test(expected=ConstraintViolationException.class)
+	public void partnerCanBePersistedWithoutFirstName() throws Exception{
+		Partner partner = new Partner();
+		partner.setLastName("Captain");
+		partner.setAirline(new Airline("test"));
+		partner.setEmailAddress("test");
+		partner.setPassword("test");
+		partner = partnerRepository.save(partner);
+		assertNotNull(partner);		
+	}
+	
+	@Test(expected=ConstraintViolationException.class)
+	public void partnerCanBePersistedWithoutLastName() throws Exception{
+		Partner partner = new Partner();
+		partner.setFirstName("Jack");
+		partner.setAirline(new Airline("test"));
+		partner.setEmailAddress("test");
+		partner.setPassword("test");
+		partner = partnerRepository.save(partner);
+		assertNotNull(partner);		
+	}
+	
+	@Test(expected=ConstraintViolationException.class)
+	public void partnerCanBePersistedWithoutEmail() throws Exception{
+		Partner partner = new Partner();
+		partner.setLastName("Captain");
+		partner.setAirline(new Airline("test"));
+		partner.setFirstName("Jack");
+		partner.setPassword("test");
+		partner = partnerRepository.save(partner);
+		assertNotNull(partner);		
+	}
+	
+	@Test(expected=ConstraintViolationException.class)
+	public void partnerCanBePersistedWithoutAirline() throws Exception{
+		Partner partner = new Partner();
+		partner.setLastName("Captain");
+		partner.setEmailAddress("test");
+		partner.setPassword("test");
+		partner = partnerRepository.save(partner);
+		assertNotNull(partner);		
+	}
+	
+	@Test(expected=ConstraintViolationException.class)
+	public void partnerCanBePersistedWithoutPassword() throws Exception{
+		Partner partner = new Partner();
+		partner.setLastName("Captain");
+		partner.setAirline(new Airline("test"));
+		partner.setEmailAddress("test");
+		partner.setFirstName("Jack");
+		partner = partnerRepository.save(partner);
+		assertNotNull(partner);		
+	}
+	
+	@Test
+	public void airportCanBeRemoved() throws Exception{
+		Partner partner1 = new Partner("captain","jack","captain@jack.com","blablabla",new Airline("SN Brussels Airlines"));
+		Partner partner2 = new Partner("captain","jack","captain@jack.com","blablabla",new Airline("SN Brussels Airlines"));
+		partnerRepository.save(partner1);
+		partnerRepository.save(partner2);
+		partnerRepository.remove(partner2.getId());
+		List<Partner> partnerList = partnerRepository.findAll();
+		assertEquals(1, partnerList.size());
+	}
+	
+	@Test
+	public void airportCanBeUpdated() throws Exception{
+		Partner partner1 = new Partner("captain","jack","captain@jack.com","blablabla",new Airline("SN Brussels Airlines"));
+		Partner partner2 = new Partner("captain","jack","captain@jack.com","blablabla",new Airline("SN Brussels Airlines"));
+		partnerRepository.save(partner1);
+		partnerRepository.save(partner2);
+		partner2.setLastName("Jane");
+		partner2 = partnerRepository.update(partner2, partner2.getId());
+		assertEquals("Jane",partner2.getLastName());
+	}
 
 }
