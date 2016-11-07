9b7a2c64992b0a46fd915b99e8c472300c14ff82
Lennart Van Damme <Lennart.VanDamme@realdolmen.com>
2016-11-07 15:57:00 +0100

    flight repo and flight admin page start

--- src/main/java/com/rair/dao/AirlineRepository.java
+++ src/main/java/com/rair/dao/AirlineRepository.java
@@ -1,10 +1,13 @@
 package com.rair.dao;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import com.rair.domain.Airline;
+import com.rair.domain.Airport;
 
 @Stateless
 public class AirlineRepository {
@@ -25,6 +28,12 @@ public class AirlineRepository {
 	public Airline retrieveAirline(String name) {
 		Airline airline = (Airline) entityManager.createQuery("SELECT a FROM Airline a WHERE a.name= '"+ name+"'").getSingleResult();
 		return airline;
+	}
+	
+	public List<Airline> retrieveAllAirports() {
+		List<Airline> airlines = entityManager.createQuery("Select airline from Airline airline", Airline.class).getResultList();
+		System.out.println(airlines);
+		return airlines;
 	}
 	
 	public void saveAirline(Airline airline) {
