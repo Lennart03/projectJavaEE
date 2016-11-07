bd7865eb73b8bd47fa6319da2589fe49952d755a
Lennart Van Damme <Lennart.VanDamme@realdolmen.com>
2016-11-07 07:59:36 +0100

    test core classes

---
+++ src/test/java/com/rair/dao/FlightRepoTest.java
@@ -0,0 +1,11 @@
+package com.rair.dao;
+
+import javax.persistence.EntityManager;
+
+import com.rair.testcore.JpaPersistenceTest;
+
+public class FlightRepoTest extends JpaPersistenceTest {
+	
+	EntityManager entityManager = entityManager();
+
+}
