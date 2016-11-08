0f5d78fab7e95b48d213981e4a69148fa5417a43
Niels Stassijns <niels.stassijns@realdolmen.com>
2016-11-08 09:18:53 +0100

    airport xhtml

--- src/main/java/com/rair/jsfbeans/AirportRetrieveBean.java
+++ src/main/java/com/rair/jsfbeans/AirportRetrieveBean.java
@@ -1,18 +1,26 @@
 package com.rair.jsfbeans;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+
+import org.primefaces.event.CellEditEvent;
+import org.primefaces.event.RowEditEvent;
 
 import com.rair.dao.AirportRepository;
 import com.rair.domain.Airport;
+import com.rair.domain.Region;
 
 @ManagedBean
 @SessionScoped
-public class AirportRetrieveBean{
+public class AirportRetrieveBean implements Serializable{
 	
 	@Inject
 	AirportRepository airportRepo;
@@ -24,6 +32,21 @@ public class AirportRetrieveBean{
 		this.airports = airportRepo.findAll();
 		return airports;
 	}
+	
+	public void onRowEdit(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("Airport Edited", ((Airport) event.getObject()).getId().toString());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+     
+    public void onRowCancel(RowEditEvent event) {
+        FacesMessage msg = new FacesMessage("Edit Cancelled", ((Airport) event.getObject()).getId().toString());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    
+    public List<Region> getRegions(){
+    	List<Region> regionList = Arrays.asList(Region.values());
+    	return regionList;
+    }
 	
 
 }
